42ea61b7b530f0a2a0d8b8060a7ffd325ac896f2
ryancblocker <ryancblocker@icloud.com>
2023-12-02 15:41:22 -0700

    finish sendFile.py

--- sendFile.py
+++ sendFile.py
@@ -3,68 +3,56 @@ import os
 import schedule
 import time
 
-def file_already_exists(sftp, remote_directory, filename):
-    try:
-        sftp.stat(os.path.join(remote_directory, filename))
-        return True
-    except IOError:
-        return False
+copy_to_remote = []
+copied_to_remote = []
 
-def copy_files_to_remote(ssh_client, local_directory, remote_directory):
-    print("Starting to copy files...")
-    new_files_found = False
+local_directory = '/Users/ryanblocker/fileBackup'
 
-    for filename in os.listdir(local_directory):
-        local_path = os.path.join(local_directory, filename)
-
-        if os.path.isfile(local_path) and not file_already_exists(ssh_client, remote_directory, filename):
-            new_files_found = True
-            print(f"Copying {filename} to remote directory...")
-            ssh_client.put(local_path, os.path.join(remote_directory, filename))
-            print(f"{filename} successfully copied.")
-        else:
-            print(f"Skipped {filename}, already exists in remote directory.")
-
-    return new_files_found
-
+def get_new_files(local_directory, copy_to_remote, copied_to_remote):
+    all_files = [f for f in os.listdir(local_directory) if os.path.isfile(os.path.join(local_directory, f))]
+    
+    for file in all_files:
+        if file not in copied_to_remote:
+            copy_to_remote.append(file)
+            
+    print(copy_to_remote)
+    
 def job():
-    print("Job started.")
+    
+    get_new_files(local_directory, copy_to_remote, copied_to_remote)
+
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    print("SSH client setup complete.")
+    ssh_client.connect(hostname='moscow.cs.colostate.edu', username='rblocker', password='RCB+thecsbuilding')
+    sftp_client = ssh_client.open_sftp()
 
-    # Replace with your details
-    host = "denver.cs.colostate.edu"
-    port = 22
-    username = "rblocker"
-    password = "RCB+thecsbuilding"
+    for file in copy_to_remote:
+        local_file_path = os.path.join(local_directory, file)
+        remote_file_path = '/s/bach/j/under/rblocker/fileBackup/' + file
+        sftp_client.put(local_file_path, remote_file_path)
+        copy_to_remote.remove(file)
+        copied_to_remote.append(file)
+    
+    sftp_client.close()
+    ssh_client.close()
+    
+    
+def main():
+    print("process start")
+    
+    schedule.every().hour.do(job)
 
-    try:
-        print(f"Attempting to connect to {host}...")
-        ssh_client.connect(hostname=host, port=port, username=username, password=password)
-        print("SSH connection established.")
-        sftp = ssh_client.open_sftp()
-
-        # Define your source and target directories
-        local_directory = '/Users/ryanblocker/fileBackup'
-        remote_directory = '/s/bach/j/rblocker/fileBackup'
-
-        if not copy_files_to_remote(sftp, local_directory, remote_directory):
-            print("No new files to copy. Stopping the program.")
-            return
-
-        print("File transfer complete.")
-
-    except Exception as e:
-        print(f"Connection Failed: {e}")
-    finally:
-        ssh_client.close()
-        print("SSH client closed.")
-
-# Schedule the job every hour
-schedule.every().second.do(job)
-print("Job scheduled to run every hour.")
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    
+    
+if __name__ == "__main__":
+    main()
+    
+        
+    
+    
+    
+    
+    
