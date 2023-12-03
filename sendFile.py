808806c4dd8956242e7d4722ae24a842bb9fbbb3
Emma Shneidman <emmars@colostate.edu>
2023-12-03 12:47:12 -0700

    Add code to copy from remote to local

--- sendFile.py
+++ sendFile.py
@@ -6,9 +6,33 @@ import time
 copy_to_remote = []
 copied_to_remote = []
 
-local_directory = '/Users/ryanblocker/fileBackup'
+remote_directory = '/s/bach/l/under/emmars/BackupFiles/'
+local_directory = '/Users/emmashneidman/BackupFiles/'
+
+def copy():
+    # Connect to client
+    ssh_client = paramiko.SSHClient()
+    ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    ssh_client.connect(hostname = 'moscow.cs.colostate.edu', username='emmars', password='dexter1234554321')
+    sftp_client = ssh_client.open_sftp()
+    
+    #Change directory to BackupFiles folder
+    sftp_client.chdir(remote_directory)
+    files = sftp_client.listdir(remote_directory)
+    
+    #Copy files from remote directory to local directory
+    for file in files:
+        remote_file_path = os.path.join(remote_directory, file)
+        local_file_path = os.path.join(local_directory, file)
+        sftp_client.get(remote_file_path, local_file_path)
+
+    ssh_client.close()
+    
+    print("Done copying from remote")
+
 
 def get_new_files(local_directory, copy_to_remote, copied_to_remote):
+    #Loop through files in local directory
     all_files = [f for f in os.listdir(local_directory) if os.path.isfile(os.path.join(local_directory, f))]
     
     for file in all_files:
@@ -19,16 +43,22 @@ def get_new_files(local_directory, copy_to_remote, copied_to_remote):
     
 def job():
     
+    #Copy from remote
+    copy()
+    
+    #Get files from local
     get_new_files(local_directory, copy_to_remote, copied_to_remote)
-
+    
+    #Connect to client 
     ssh_client = paramiko.SSHClient()
     ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    ssh_client.connect(hostname='moscow.cs.colostate.edu', username='rblocker', password='RCB+thecsbuilding')
+    ssh_client.connect(hostname='dragonfly.cs.colostate.edu', username='emmars', password='dexter1234554321')
     sftp_client = ssh_client.open_sftp()
 
+    #Copy files from local to remote
     for file in copy_to_remote:
         local_file_path = os.path.join(local_directory, file)
-        remote_file_path = '/s/bach/j/under/rblocker/fileBackup/' + file
+        remote_file_path = '/s/bach/l/under/emmars/Filebackup/' + file
         sftp_client.put(local_file_path, remote_file_path)
         copy_to_remote.remove(file)
         copied_to_remote.append(file)
@@ -40,7 +70,7 @@ def job():
 def main():
     print("process start")
     
-    schedule.every().hour.do(job)
+    schedule.every().second.do(job)
 
     while True:
         schedule.run_pending()
